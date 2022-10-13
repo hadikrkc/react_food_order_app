73031114733f9495fe974ccda9b7de846c0aa3b2
Hadi KIRKICI <abdulhadi.kirkici@vestel.com.tr>
2022-10-13 16:54:00 +0300

    Adding Individual MEal Items & Display Them

--- src/componenets/Meals/AvailableMeals.js
+++ src/componenets/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvaliableMeals.module.css'
 
 const DUMMY_MEALS = [
@@ -28,12 +30,22 @@ const DUMMY_MEALS = [
   ];
 
 const AvaliableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) => (
+        <MealItem             
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        > 
+        </MealItem>
+    ));
     return (
         <section className={classes.meals}>
-            <ul>
-                <li>{mealsList}</li>
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
