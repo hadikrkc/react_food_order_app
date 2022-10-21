312a1aa95698b62eca6e1e68ef45ebeda9078561
Hadi KIRKICI <abdulhadi.kirkici@vestel.com.tr>
2022-10-21 11:47:46 +0300

    Adding a Modal via a React Portal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Fragment } from 'react';
 import Header from './componenets/Layout/Header';
 import Meals from './componenets/Meals/Meals';
+import Cart from './componenets/Cart/Cart';
+
 function App() {
   return (
     <Fragment >
+      <Cart></Cart>
       <Header></Header>
       <div>
           <Meals></Meals>
