d0a2b485d593553a1bba4753046e71664e4a8da6
Hadi KIRKICI <abdulhadi.kirkici@vestel.com.tr>
2022-12-23 15:06:51 +0300

    Making Items Removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,7 +9,7 @@ const cartReducer = (state, action) => {
     if(action.type === 'ADD'){
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
-        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
         const existingCartItem = state.items[existingCartItemIndex];
         let updatedItems;
 
@@ -18,6 +18,7 @@ const cartReducer = (state, action) => {
                 ...existingCartItem,
                 amount: existingCartItem.amount + action.item.amount
             };
+            updatedItems = [...state.items];
             updatedItems[existingCartItemIndex] = updatedItem;
         }else{
             updatedItems = state.items.concat(action.item);
@@ -28,6 +29,29 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount
         };
     }
+
+    if(action.type === 'REMOVE'){
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+        let updatedItems;
+
+        if(existingItem.amount === 1){
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        }else{
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return{
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
     return defaultCartState;
 };
 
